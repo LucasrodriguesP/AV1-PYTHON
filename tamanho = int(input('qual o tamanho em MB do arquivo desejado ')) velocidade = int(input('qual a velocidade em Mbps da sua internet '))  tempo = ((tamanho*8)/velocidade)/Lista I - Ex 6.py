c1ffdeb774969b6ba56e9b5679caceb27aad1612
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-09-29 17:59:35 -0300

    Delete tamanho = int(input('qual o tamanho em MB do arquivo desejado ')) velocidade = int(input('qual a velocidade em Mbps da sua internet '))  tempo = ((tamanho*8)/velocidade) directory

--- tamanho = int(input('qual o tamanho em MB do arquivo desejado ')) velocidade = int(input('qual a velocidade em Mbps da sua internet '))  tempo = ((tamanho*8)/velocidade)/Lista I - Ex 6.py
+++
@@ -1,6 +0,0 @@
-tamanho = int(input('qual o tamanho em MB do arquivo desejado '))
-velocidade = int(input('qual a velocidade em Mbps da sua internet '))
-
-tempo = ((tamanho*8)/velocidade)/60
-
-print('Aproximadamente %.2f minutos para baixar' %tempo)
