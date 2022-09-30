72b4a5a8c98898e3e50aa787cbcb4564435b7d13
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-09-30 13:10:46 -0300

    Update Lista I - Ex 1.py

--- Trabalho AV1/Lista I - Ex 1.py
+++ Trabalho AV1/Lista I - Ex 1.py
@@ -1,3 +1,9 @@
+#Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu 
+#peso ideal, utilizando as seguintes fórmulas:
+#a. Para homens: (72.7*h) - 58
+#b. Para mulheres: (62.1*h) - 44.7
+
+
 altura = float(input('Digite sua altura em metros usando ponto [.]'))
 sexo = int(input('1 para homem 2 para mulher'))
 
