846bcfe32b52f89760de12e9340a1cb5a4e810f9
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-09-30 13:08:15 -0300

    Update Lista I - Ex 6.py

--- Trabalho AV1/Lista I - Ex 6.py
+++ Trabalho AV1/Lista I - Ex 6.py
@@ -1,3 +1,8 @@
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de 
+#um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo 
+#usando este link (em minutos)
+
+
 tamanho = int(input('qual o tamanho em MB do arquivo desejado '))
 velocidade = int(input('qual a velocidade em Mbps da sua internet '))
 
