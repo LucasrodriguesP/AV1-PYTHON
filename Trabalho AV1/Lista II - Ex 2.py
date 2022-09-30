2216d5b000ce0319b4a5d9601b34d72c8849da08
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-09-30 13:06:40 -0300

    Update Lista II - Ex 2.py

--- Trabalho AV1/Lista II - Ex 2.py
+++ Trabalho AV1/Lista II - Ex 2.py
@@ -1,3 +1,6 @@
+#2. Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar.
+
+
 numero = int(input('Digite um numero inteiro'))
 if (numero % 2 != 0):
     print('numero digitado eh impar')
